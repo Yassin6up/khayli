db86d056696fc9e9d4f6e4e1096b6222dc1c75bd
Your Name <you@example.com>
2024-04-01 14:42:56 +0000

    add some sections

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 function toggleDropdown() {
     var dropdown = document.getElementById("dropdown");
     dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
@@ -93,3 +98,22 @@ Array.from(cardsInModal).forEach((card)=>{
         }
     }
 })
+
+
+
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
